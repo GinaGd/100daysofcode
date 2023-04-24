29203bb61e8ec50589538a579b5825b77254b96b
ginagdigital <88678839+GinaGd@users.noreply.github.com>
2023-04-24 21:09:24 +0200

    new practise

---
+++ code react/Appwkd.js
@@ -0,0 +1,19 @@
+Import React from 'react';
+import ReactDOM from 'react-dom';
+
+import WeekEnd from './weekend';
+
+function MeTime() {
+    return <WeekEnd mood="🍿🎬🛀"/>;
+}
+
+export default MeTime;
+
+
+import WeekEnd from './weekend';
+
+function MeTime() {
+    return <WeekEnd mood="⏰⌚️🙇🔍💻"/>;
+}
+
+export default MeTime;
---
+++ code react/weekend.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function WeekEnd(props) {
+  return <h1>{props.mood}</h1>;
+}
+
+export default MyComponent;
